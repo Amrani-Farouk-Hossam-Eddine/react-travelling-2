5340734ba6298333cf0c19e0569c6dc92cc8ee57
Amrani-Farouk-Hossam-Eddine <119071864+Amrani-Farouk-Hossam-Eddine@users.noreply.github.com>
2023-08-14 18:35:44 +0200

    first commit

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,7 +4,13 @@ import { FaSearch } from "react-icons/fa";
 const Hero = () => {
   return (
     <div className="h-[100vh] mt-[-80px]">
-      <video src={Video} loop className="h-full w-full object-cover"></video>
+      <video
+        src={Video}
+        autoPlay
+        muted
+        loop
+        className="h-full w-full object-cover"
+      ></video>
       <div className="w-full h-full absolute top-0 left-0 bg-black/30"></div>
       <div className="absolute max-w-[600px] w-full top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] mx-auto z-5 text-center text-white">
         <h2 className="text-4xl font-bold">First Class Travel</h2>
