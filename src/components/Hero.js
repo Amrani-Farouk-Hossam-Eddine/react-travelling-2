143fd3ab10ab5fe7022622f5e3c29b1c2b25ff5e
Amrani-Farouk-Hossam-Eddine <119071864+Amrani-Farouk-Hossam-Eddine@users.noreply.github.com>
2023-08-14 18:47:55 +0200

    first commit

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -12,7 +12,7 @@ const Hero = () => {
         className="h-full w-full object-cover"
       ></video>
       <div className="w-full h-full absolute top-0 left-0 bg-black/30"></div>
-      <div className="absolute max-w-[600px] w-full top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] mx-auto z-5 text-center text-white">
+      <div className="absolute max-w-[300px] sm:max-w-[600px] w-full top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] mx-auto z-5 text-center text-white">
         <h2 className="text-4xl font-bold">First Class Travel</h2>
         <p className="py-4 font-bold text-2xl">Top 1% Locations WorldWide</p>
         <form className="w-full bg-white p-2 flex justify-between rounded-md">
