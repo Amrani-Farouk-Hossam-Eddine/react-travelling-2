71c5b3726e9b8e8c4a701527cf91e73cd3b85772
Amrani-Farouk-Hossam-Eddine <119071864+Amrani-Farouk-Hossam-Eddine@users.noreply.github.com>
2023-08-14 19:16:27 +0200

    first commit

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -13,11 +13,13 @@ const Hero = () => {
       ></video>
       <div className="w-full h-full absolute top-0 left-0 bg-black/30"></div>
       <div className="absolute max-w-[270px] sm:max-w-[600px] w-full top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] mx-auto z-5 text-center text-white">
-        <h2 className="text-4xl font-bold">First Class Travel</h2>
-        <p className="py-4 font-bold text-2xl">Top 1% Locations WorldWide</p>
-        <form className="w-full bg-white p-2 flex justify-between rounded-md">
+        <h2 className="text-2xl sm:text-4xl font-bold">First Class Travel</h2>
+        <p className="py-4 font-bold text-xl sm:text-2xl">
+          Top 1% Locations WorldWide
+        </p>
+        <form className="w-full bg-white p-2 flex flex-col sm:flex-row justify-between rounded-md">
           <input type="text" placeholder="Search Destinations" />
-          <button className="rounded-md">
+          <button className="rounded-md grid place-content-center">
             <FaSearch />
           </button>
         </form>
